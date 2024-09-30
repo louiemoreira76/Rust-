a91be6a821a02e363e3b7a4b60bf04a52a8a84ed
Luis Moreira <luis010101work@outlook.com>
2024-09-30 16:13:43 -0300

    Foi?

--- target/debug/deps/funfa-de83736a3caa9a3f.d
+++ target/debug/deps/funfa-de83736a3caa9a3f.d
@@ -1,5 +1,5 @@
-C:\Users\£ui$\rust\funfa\target\debug\deps\libfunfa-de83736a3caa9a3f.rmeta: src\main.rs
+C:\Users\£ui$\Documents\CS50\Rust-\target\debug\deps\libfunfa-de83736a3caa9a3f.rmeta: src\main.rs
 
-C:\Users\£ui$\rust\funfa\target\debug\deps\funfa-de83736a3caa9a3f.d: src\main.rs
+C:\Users\£ui$\Documents\CS50\Rust-\target\debug\deps\funfa-de83736a3caa9a3f.d: src\main.rs
 
 src\main.rs:
--- target/debug/deps/rand_chacha-2452c24145956e79.d
+++ target/debug/deps/rand_chacha-2452c24145956e79.d
@@ -1,6 +1,6 @@
-C:\Users\£ui$\rust\funfa\target\debug\deps\librand_chacha-2452c24145956e79.rmeta: C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+C:\Users\£ui$\Documents\CS50\Rust-\target\debug\deps\librand_chacha-2452c24145956e79.rmeta: C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
 
-C:\Users\£ui$\rust\funfa\target\debug\deps\rand_chacha-2452c24145956e79.d: C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+C:\Users\£ui$\Documents\CS50\Rust-\target\debug\deps\rand_chacha-2452c24145956e79.d: C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
 
 C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs:
 C:\Users\£ui$\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs:
